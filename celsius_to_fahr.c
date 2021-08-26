780b8d4cbc95803170790fc9c3d6ea0e70c47cd8
Dave Masino <davemasino@hotmail.com>
2021-08-25 21:19:31 -0700

    Format fix

--- celsius_to_fahr.c
+++ celsius_to_fahr.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table
-    for fahr = 0, 20, ..., 300 */
+/* print Celsius-Fahrenheit table
+    for celsius = 0, 20, ..., 110 */
 
 int main()
 {
@@ -16,7 +16,7 @@ int main()
     celsius = lower;
     while (celsius <= upper) {
         fahr= (9.0/5.0 * celsius) + 32.0;
-        printf("%3.0f\t%3.1f\n", celsius, fahr);
+        printf("%3.0f\t%6.1f\n", celsius, fahr);
         celsius = celsius + step;
     }
     return 0;
