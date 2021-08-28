a6b3ab6f7b8fa66a61075a30317f714e4360a426
Dave Masino <davemasino@hotmail.com>
2021-08-27 21:41:40 -0700

    char count v2

--- chapter1/char_count.c
+++ chapter1/char_count.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-/* count chars in input; v1 */
+/* count chars in input; v2 */
 int main ()
 {
-    long nc;
-    
-    nc = 0;
-    while (getchar() != EOF) 
-        ++nc;
-    printf("%ld\n", nc);
+    double nc;
+    for (nc = 0; getchar() != EOF ; ++nc)
+        ;
+
+    printf("%0.f\n", nc);
 }
